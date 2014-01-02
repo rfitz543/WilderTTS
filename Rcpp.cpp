91db75d620cc26ea2c0878d5ed8d04444cae34d7
rfitz543 <rfitz543@gmail.com>
2014-01-01 19:11:26 -0500

    Update Rcpp.cpp

--- Rcpp.cpp
+++ Rcpp.cpp
@@ -27,11 +27,13 @@ NumericVector run_mean(NumericVector x, int n) {
 }
 
 // [[Rcpp::export]]
-NumericVector run_min(NumericVector x, int n){
+NumericVector run_min(NumericVector x, int n)
+{
     int sz = x.size();
     NumericVector res(sz);
     
-    for(int i = 0; i < (sz-n+1); i++){
+    for(int i = 0; i < (sz-n+1); i++)
+    {
         res[i+n-1] = *std::min_element(x.begin() + i, x.end() - sz + n + i);
         cout << i << endl;
     }
@@ -41,13 +43,33 @@ NumericVector run_min(NumericVector x, int n){
 }
 
 // [[Rcpp::export]]
-NumericVector run_max(NumericVector x, int n){
+NumericVector run_max(NumericVector x, int n)
+{
     int sz = x.size();
     NumericVector res(sz);
-    for(int i = 0; i < (sz-n+1); i++){
+    for(int i = 0; i < (sz-n+1); i++)
+    {
         res[i+n-1] = *std::max_element(x.begin() + i, x.end() - sz + n + i);
     }
     // pad the first n-1 elements with NA
     std::fill(res.begin(), res.end()-sz+n-1, NA_REAL);
     return res;
+}
+
+// [[Rcpp:export]]
+NumericVector true_range(NumericMatrix x, int n)
+{
+  int sz = x.nrow();
+  int hi = 1;
+  int lo = 2;
+  int cl = 3;
+  
+  NumericVector vec(sz);
+  vec[0] = x(0,hi)-x(0,lo);
+  for(int i=1; i<sz; i++)
+  {
+    vec[i] = std::max(x(i,hi)-x(i,lo),std::max(std::abs(x(i,hi)-x(i-1,cl)),
+    std::abs(x(i,lo)-x(i-1,cl))));
+  }
+  return(vec);
 }
