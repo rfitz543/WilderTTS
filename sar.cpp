e5a6498e1509a4c989905b7e23fe3860d548e88c
rfitz543 <rfitz543@gmail.com>
2014-01-21 23:16:56 -0500

    Update sar.cpp

--- sar.cpp
+++ sar.cpp
@@ -35,3 +35,34 @@ NumericVector quick_sar_long(NumericMatrix x, double stop)
     
     return sar;
 }
+
+// [[Rcpp::export]]
+NumericVector quick_sar_short(NumericMatrix x, double stop) 
+{
+    int lo = 2;
+    
+    int sz = x.nrow();
+    NumericVector ep(sz);
+    NumericVector af(sz);
+    NumericVector sar(sz);
+    
+    ep[0] = x(0, lo);
+    sar[0] = stop;
+    af[0] = .02;
+    for(int i=1; i<sz; i++)
+    {
+      ep[i] = std::min(ep[i-1], x(i, lo));
+      sar[i] = std::floor((sar[i-1] + af[i-1]*(ep[i-1]-sar[i-1]))*100+.5)/100;
+       
+      if(ep[i]<ep[i-1])
+      {
+        af[i] = std::min(af[i-1]+.02, .2);
+      }
+      else
+      {
+        af[i] = af[i-1];
+      }
+    }
+    
+    return sar;
+}
