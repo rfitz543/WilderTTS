0f1fc0d5f3f584a9d151377ec85187312a995ea9
rfitz543 <rfitz543@gmail.com>
2014-01-21 19:41:26 -0500

    Create sar.R
    Welle's Wilder Stop and Reverse

---
+++ sar.R
@@ -0,0 +1,37 @@
+#include <Rcpp.h>
+#include <numeric>      // for accumulate
+
+using namespace Rcpp;
+using namespace std;
+
+// [[Rcpp::export]]
+NumericVector quick_sar_long(NumericMatrix x, double stop) 
+{
+    int hi = 1;
+    int lo = 2;
+    
+    int sz = x.size();
+    NumericVector ep(sz);
+    NumericVector af(sz);
+    NumericVector sar(sz);
+    
+    ep[0] = x(0, hi);
+    sar[0] = stop;
+    af[0] = .02;
+    for(int i=1; i<sz; i++)
+    {
+      sar[i] = sar[i-1] + af[i-1]*(x(i, hi)-sar[i-1]);
+      ep[i] = std::max(x.begin(), x.end()-sz+i);
+      af[i] = 
+      if(ep[i]!=ep[i-1])
+      {
+        std::min(af[i-1]+.02, .2);
+      }
+      else
+      {
+        af[i-1];
+      }
+    }
+    
+    return sar;
+}
