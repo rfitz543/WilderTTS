028f9c3fa9cefc6e2739f3b94f1e7ce2f41dfb6b
rfitz543 <rfitz543@gmail.com>
2014-01-21 23:10:59 -0500

    Update sar.cpp

--- sar.cpp
+++ sar.cpp
@@ -23,7 +23,7 @@ NumericVector quick_sar_long(NumericMatrix x, double stop)
       ep[i] = std::max(ep[i-1], x(i, hi));
       sar[i] = std::floor((sar[i-1] + af[i-1]*(ep[i-1]-sar[i-1]))*100+.5)/100;
        
-      if(ep[i]!=ep[i-1])
+      if(ep[i]>ep[i-1])
       {
         af[i] = std::min(af[i-1]+.02, .2);
       }
